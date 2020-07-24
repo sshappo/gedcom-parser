17d743d26327b2596368861c5331838aa1b14913
Sergey Shappo <srgshpp@gmail.com>
2020-07-24 22:54:14 +0200

    add parse date of death

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import {GedcomParser} from "./GedcomParser";
+
 export {GedcomParser} from "./GedcomParser";
 export {BaseParser} from "./BaseParser";
 export {Person} from "./Person";
 export {Row} from "./Row";
+
+
+const gedcom = require("./111.ged").default;
+console.time('GedcomParser');
+const gedcomParser = new GedcomParser(gedcom);
+console.log(gedcomParser.data);
+console.timeEnd('GedcomParser');
